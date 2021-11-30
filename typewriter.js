94cc4027d46de3a4ab58882050c8a6bd65bbe7fd
Ray Flores <ray.m.flores@gmail.com>
2021-11-30 17:36:38 +0000

    completed typewriter.js exercise including modifications to setTimeout function

---
+++ typewriter.js
@@ -0,0 +1,16 @@
+const sentence = "hello there from lighthouse labs";
+let arr = sentence.split('');
+
+let start = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(arr[i]);
+    if (i === (arr.length - 1)) {
+      process.stdout.write('\n');
+    }
+  }, (start += 50));
+}
+
+
+
